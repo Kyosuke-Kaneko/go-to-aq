210d91b014ea21c10ae7cbb6169df3e51d262e3b
AQ-Kyosuke-Kaneko <kyosuke.kaneko@asia-quest.jp>
2022-04-20 14:33:00 +0900

    [add]v-navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,43 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-navigation-drawer
+                app
+                v-model="drawer"
+                clipped
+        >Navigation Lists
+        </v-navigation-drawer>
+        <v-app-bar
+                color="primary"
+                dark
+                :collapse="!collapseOnScroll"
+                :collapse-on-scroll="collapseOnScroll"
+                elevate-on-scroll
+                app
+                clipped-left>
+            <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+            <v-checkbox
+                    v-model="collapseOnScroll"
+                    color="white"
+                    hide-details
+            ></v-checkbox>
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+        </v-app-bar>
+        <v-footer color="primary" dark app>
+            Vuetify
+        </v-footer>
+        <v-container style="height: 1500px;">
+        </v-container>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  data() {
+    return {
+      collapseOnScroll: true,
+      // clipped: true,
+      drawer: null
+    }
+  }
+}
 </script>
